49455263319787a2b201491723685729b4bfc80e
Evan Xiang <evankxiang@gmail.com>
2024-08-12 17:17:42 -0400

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
+  site: 'evankxiang.netlify.app',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
